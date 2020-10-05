f2413acc8e87c3166a0db4080dc6d335f169c936
Daniel Georges <dannnn.georges@gmail.com>
2020-10-05 11:29:20 -0300

    Configuração para persistência das senhas de forma criptografada

--- src/main/java/br/com/casadocodigo/loja/controllers/UsuarioController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/UsuarioController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
@@ -50,7 +51,9 @@ public class UsuarioController {
 		} else if (result.hasErrors()) {
 			return form(usuario);
 		}
-
+		String senhaCriptografada = new BCryptPasswordEncoder().encode(usuario.getSenha());
+		usuario.setSenha(senhaCriptografada);
+		
 		usuarioDao.gravar(usuario);
 
 		redirectAttributes.addFlashAttribute("message", "Usuário cadastrado com sucesso");
